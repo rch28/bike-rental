fb3b68958ce9a5f1a145ca96768a6a25426d706a
iamxettry <rajuchhetry11@gmail.com>
2024-08-28 15:40:21 +0545

    init

---
+++ src/lib/Style.js
@@ -0,0 +1,11 @@
+export const Style = {
+    bgPrimary:
+      "bg-gradient-to-r from-blue-300 to-pink-100 dark:bg-gradient-to-r dark:from-black dark:to-black",
+    transiton: "transition duration-500 ease-linear",
+    dashlineH:
+      "w-40 border-black  border-dashed border-b dark:border-white absolute",
+    dashlineV:
+      "h-40 border-black  border-dashed border-l dark:border-white absolute ",
+    backlight:
+      "w-24 h-24  bg-gradient-to-r -z-10  blur-[30px] dark:blur-[40px] rounded-full  absolute ",
+  };
---
+++ src/lib/route.ts
@@ -0,0 +1,40 @@
+/**
+ * @description Public routes
+ * An array of routes that are public and do not require authentication
+ * 
+ * @type {string[]}
+ */
+export const publicRoutes = [
+    "/",
+];
+
+/**
+ * @description Routes that require authentication
+ * An array of routes that are used for the authentication process
+ * These routes will redirect loggedin users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/login",
+    "/auth/login/verify-otp",
+    "/auth/register",
+    "/auth/forgot-password",
+    "/auth/reset-password",
+    "/auth/forgot-password/verify-otp",
+    "/auth/error",
+];
+
+/**
+ * @description Prefix for all auth routes
+ * Routes that start with this prefix are  used for API authentication process
+ * @type {string}
+ */
+
+export const apiAuthPrefix="/api/auth";
+
+/**
+ * @description Default login redirect
+ * The default route to redirect to after a successful login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT="/";
