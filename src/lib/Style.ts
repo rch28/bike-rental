88706a83958d09c7d720345db694069b6c46051c
iamxettry <rajuchhetry11@gmail.com>
2024-08-28 18:01:44 +0545

    fix : text-color

--- src/lib/Style.ts
+++ src/lib/Style.ts
@@ -1,6 +1,6 @@
 export const Style = {
     bgPrimary:
-      "bg-gradient-to-r from-blue-300 to-pink-100 dark:bg-gradient-to-r dark:from-black dark:to-black",
+      "bg-gradient-to-r from-blue-300 to-pink-100 dark:bg-gradient-to-r dark:from-black dark:to-black text-black dark:text-white",
     transiton: "transition duration-500 ease-linear",
     dashlineH:
       "w-40 border-black  border-dashed border-b dark:border-white absolute",
