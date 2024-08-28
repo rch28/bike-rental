770b7f0be4b24929eaa3b1e7624278fbf7faab3d
iamxettry <rajuchhetry11@gmail.com>
2024-08-28 17:23:51 +0545

    feat: footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Style } from "@/lib/Style";
+import Footer from "@/components/global/Footer";
 export const metadata: Metadata = {
   title: "Bike Rental System",
   description: "Bike Rental System",
@@ -13,7 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={Style.bgPrimary}>
+        <div className="flex flex-col min-h-screen  ">
+          <nav>Navbar</nav>
+          <main className="flex-1">{children}</main>
+          <footer className={`fixed bottom-0 left-0 w-full border-t border-neutral-400 ${Style.bgPrimary} p-2`}><Footer/></footer>
+        </div>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode:"class",
   theme: {
     extend: {
       backgroundImage: {
