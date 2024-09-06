0f22d25122416dd1b0ba29768ccca1324173b5b6
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-06 11:43:24 +0545

    feat : added new logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,8 @@
 import WithMyBike from './logo/brslogo.png';
 import HeroImg from "./hero.jpg"
 
+import Money from './logo/money.svg';
+import QualityService from './logo/quality-service.svg';
+import Booking from './logo/booking.svg'
 
-export { WithMyBike, HeroImg };
+export { WithMyBike, HeroImg, Money, QualityService, Booking };
