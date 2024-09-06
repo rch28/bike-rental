5c85bd36a1980f3ad6972af110afc40ebd26027c
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-06 13:50:02 +0545

    About Images added

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,6 @@ import HeroImg from "./hero.jpg"
 import Money from './logo/money.svg';
 import QualityService from './logo/quality-service.svg';
 import Booking from './logo/booking.svg'
+import Hobby11 from "./logo/bikes/hobby_11.jpg"
 
-export { WithMyBike, HeroImg, Money, QualityService, Booking };
+export { WithMyBike, HeroImg, Money, QualityService, Booking , Hobby11};
