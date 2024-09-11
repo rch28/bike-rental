1bf64631c4e125469f358a0be01ae3f88f01668a
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-11 21:33:32 +0545

    fix : button height and width

--- src/components/utils/Button.tsx
+++ src/components/utils/Button.tsx
@@ -13,7 +13,7 @@ export const Button: React.FC<ButtonProps> = ({
       {path ? (
         <Link
           href={path}
-          className={`relative px-[22px] py-[10px] overflow-hidden min-w-20 bg-primary flex justify-center items-center rounded-xl  font-medium hover:text-white   before:absolute before:w-0 before:h-0 before:bg-orange-500 before:transition-all before:ease-linear before:duration-300 hover:before:w-60 hover:before:h-60 before:rounded-full group  ${className}`}
+          className={`relative px-[22px] py-[10px] overflow-hidden min-w-20 bg-primary flex justify-center items-center rounded-xl  font-medium hover:text-white   before:absolute before:w-0 before:h-0 before:bg-orange-500 before:transition-all before:ease-linear before:duration-300 hover:before:w-96 hover:before:h-96 before:rounded-full group  ${className}`}
         >
           <span className="relative z-10">{title}</span>
           {icon && <span className="ml-2 relative z-10 ">{icon}</span>}
