79f1c2c924afeda1fb1072442a8278c65047558e
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-16 22:40:02 +0545

    feat : added store for drawer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,20 @@
 import { create } from 'zustand'
 type State = {
-   isLogged: boolean
+   isLogged: boolean,
+   translateSiderbar: string
   }
 
 type Action = {
   setLogged: (value: boolean) => void
+  setTranslateSiderbar: (value: string) => void
     
   }
 export const useStore = create<State & Action>((set) => ({
   isLogged: false,
   setLogged: (value) => set({ isLogged: value }),
+
+  // toggle siderbar
+  translateSiderbar : '-translate-x-full',
+  setTranslateSiderbar: (value) => set({ translateSiderbar: value }),
+
 }))
