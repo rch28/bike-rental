590b5c26bb8f5e6c894c7195050b61676a70a03c
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-20 21:19:46 +0545

    fix : bike card for admin

--- src/components/global/Bike.tsx
+++ src/components/global/Bike.tsx
@@ -3,32 +3,33 @@ import { FaRoad, FaStar } from "react-icons/fa6";
 import { RiSteeringFill } from "react-icons/ri";
 import { Button } from "../utils/Button";
 
-interface bikeProps{
-  bike:Bike
+interface bikeProps {
+  bike: Bike;
+  admin: boolean;
 }
-export const Bike:React.FC<bikeProps> = ({bike}) => {
-  const {name,rating, image, features, price} = bike;
+export const Bike: React.FC<bikeProps> = ({ bike, admin = false }) => {
+  const {id, name, rating, image, features, price } = bike;
 
   return (
     <div
       className={`p-4 max-w-md group transition-all ease-in duration-300 hover:-translate-y-3 bg-gradient-to-br from-pink-100 to-blue-50 rounded-xl shadow-sm shadow-gray-300`}
     >
-      <div className=" relative rounded-xl w-full  h-60 overflow-hidden">
+      <div className={`relative rounded-xl w-full  ${admin?"":"h-60"} overflow-hidden`}>
         <img
           src={image}
           alt="bike"
-          className="w-full h-60 rounded-xl group-hover:scale-110 transition-all ease-linear duration-300  object-cover"
+          className={`w-full ${admin?"":"h-60"} rounded-xl group-hover:scale-110 transition-all ease-linear duration-300  object-cover`}
         />
       </div>
 
       <div className="flex justify-between items-center my-2 ">
-        <h1 className="text-xl font-bold capitalize">{name}</h1>
+        <h1 className={` ${admin?"":"text-xl"} font-bold capitalize`}>{name}</h1>
         <span className="flex items-center gap-1">
           <FaStar className="inline-block text-primary pb-0.5" />
           <span className="text-sm font-medium">{rating}</span>
         </span>
       </div>
-      <div className="flex flex-wrap gap-4 mt-4 text-gray-600 font-medium">
+      <div className={`flex flex-wrap gap-4 mt-4 text-gray-600 font-medium ${admin?"text-xs":""}  `}>
         <span className="flex items-center gap-2">
           <FaBiking className="text-primary" />
           <span className="uppercase">{features.start}</span>
@@ -44,10 +45,14 @@ export const Bike:React.FC<bikeProps> = ({bike}) => {
       </div>
       <div className="flex items-center justify-between my-3  border-t border-neutral-400 pt-2">
         <h2 className="flex items-center">
-          <span className="text-xl font-bold text-primary ">रु {price}</span>
+          <span className={`${admin?"text-sm":"text-xl "}font-bold text-primary`}>रु {price}</span>
           <span className="text-xs text-gray-500 font-semibold">/day</span>
         </h2>
-        <Button title="Rent Now" className=" text-white" />
+        {admin ? (
+          <Button title="Edit" path={`/admin/bikes/${id}`} className="text-white  " />
+        ) : (
+          <Button title="Rent Now" className=" text-white" />
+        )}
       </div>
     </div>
   );
