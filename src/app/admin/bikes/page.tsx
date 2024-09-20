23622d62d836be510ad0e6f49081e4da0093040d
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-20 09:34:10 +0545

    feat : bike data fetch

--- src/app/admin/bikes/page.tsx
+++ src/app/admin/bikes/page.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+"use client"
+
+import { useEffect, useState } from "react";
 
 const AdminBikesPage = () => {
+    const [bikes, setBikes] = useState([]);
+
+    useEffect(()=>{
+        const fetchBikes = async () => {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/bike/lists/`);
+            const data = await response.json();
+            setBikes(data);
+        };
+        fetchBikes()
+    },[])
   return (
     <div>AdminBikesPage</div>
   )
