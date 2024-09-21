2a93b19346a348016965b5d488a148b56de154a5
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-21 20:21:17 +0545

    feat : store to add bike drawer

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { create } from 'zustand'
 type State = {
    isLogged: boolean,
    translateSiderbar: string
+    toggleAddBikeDrawer: boolean
   }
 
 type Action = {
   setLogged: (value: boolean) => void
   setTranslateSiderbar: (value: string) => void
+  setToggleAddBikeDrawer: (value: boolean) => void
     
   }
 export const useStore = create<State & Action>((set) => ({
@@ -16,5 +18,9 @@ export const useStore = create<State & Action>((set) => ({
   // toggle siderbar
   translateSiderbar : '-translate-x-full',
   setTranslateSiderbar: (value) => set({ translateSiderbar: value }),
+
+  // toggle AddBikeDrawer
+  toggleAddBikeDrawer: false,
+  setToggleAddBikeDrawer: (value) => set({ toggleAddBikeDrawer: value }),
 
 }))
