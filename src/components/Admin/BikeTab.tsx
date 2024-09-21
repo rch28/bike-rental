38fa161217571c525432a10f1aa40587468e641f
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-21 20:22:24 +0545

    feat : add bike functionlity

--- src/components/Admin/BikeTab.tsx
+++ src/components/Admin/BikeTab.tsx
@@ -3,10 +3,16 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { Button } from "../utils/Button";
+import AddBikeDrawer from "./AddBikeDrawer";
+import { useStore } from "@/store/store";
 
 const BikeTab = () => {
   const pathname = usePathname();
-
+  const { toggleAddBikeDrawer, setToggleAddBikeDrawer } = useStore();
+  console.log(toggleAddBikeDrawer);
+  const handleClick = () => {
+    setToggleAddBikeDrawer(true);
+  };
   return (
     <nav className="flex justify-between items-center mr-6 mb-4">
       <ul className="flex items-center gap-4 rounded-lg bg-blue-200 py-2 px-4">
@@ -27,7 +33,16 @@ const BikeTab = () => {
           <Link href="/admin/featured-bikes">Featured Bikes</Link>
         </li>
       </ul>
-      <Button  title="Add New Bike" className="text-white" />
+      <Button
+        title="Add New Bike"
+        onClick={handleClick}
+        className="text-white"
+      />
+      {toggleAddBikeDrawer && (
+        <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-50 flex justify-end">
+          <AddBikeDrawer />
+        </div>
+      )}
     </nav>
   );
 };
--- src/components/utils/Button.tsx
+++ src/components/utils/Button.tsx
@@ -7,6 +7,7 @@ export const Button: React.FC<ButtonProps> = ({
   path,
   title,
   icon,
+  onClick
 }) => {
   return (
     <>
@@ -21,6 +22,7 @@ export const Button: React.FC<ButtonProps> = ({
       ) : (
         <button
           className={`relative px-[22px] py-[10px] overflow-hidden min-w-20 bg-primary flex justify-center items-center rounded-xl  font-medium hover:text-white   before:absolute before:w-0 before:h-0 before:bg-orange-500 before:transition-all before:ease-linear before:duration-300 hover:before:w-60 hover:before:h-60 before:rounded-full group  ${className}`}
+          onClick={onClick}
         >
           <span className="relative z-10">{title}</span>
           {icon && <span className="ml-2 relative z-10 ">{icon}</span>}
