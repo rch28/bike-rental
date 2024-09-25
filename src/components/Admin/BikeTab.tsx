d388f3eb5310b7fe9fea4bdf49becf9ef6fa9550
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-25 20:17:18 +0545

    fix : title

--- src/components/Admin/BikeTab.tsx
+++ src/components/Admin/BikeTab.tsx
@@ -9,30 +9,12 @@ import { useStore } from "@/store/store";
 const BikeTab = () => {
   const pathname = usePathname();
   const { toggleAddBikeDrawer, setToggleAddBikeDrawer } = useStore();
-  console.log(toggleAddBikeDrawer);
   const handleClick = () => {
     setToggleAddBikeDrawer(true);
   };
   return (
-    <nav className="flex justify-between items-center mr-6 mb-4">
-      <ul className="flex items-center gap-4 rounded-lg bg-blue-200 py-2 px-4">
-        <li
-          className={
-            pathname === "/admin/bikes" ? "bg-white px-4 py-1 rounded-lg" : ""
-          }
-        >
-          <Link href="/admin/bikes">Bikes</Link>
-        </li>
-        <li
-          className={
-            pathname === "/admin/featured-bikes"
-              ? "bg-white px-4 py-1 rounded-lg"
-              : ""
-          }
-        >
-          <Link href="/admin/featured-bikes">Featured Bikes</Link>
-        </li>
-      </ul>
+    <nav className="flex justify-between items-center mr-6 ">
+      <h1 className="text-3xl font-semibold px-2 ">Our Bike list</h1>
       <Button
         title="Add New Bike"
         onClick={handleClick}
