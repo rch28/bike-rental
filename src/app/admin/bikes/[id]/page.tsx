c3356bd619181dfad3069c7051dd1762a23096ff
Raju Chhetri <rajuchhetry11@gmail.com>
2024-10-06 22:59:20 +0545

    added image field

--- src/app/admin/bikes/[id]/page.tsx
+++ src/app/admin/bikes/[id]/page.tsx
@@ -142,14 +142,14 @@ const BikeEdit = () => {
             </label>
             <img src={bike.image} alt="" />
             {/* Add a file input or let the user provide an image URL */}
-            {/* <input
+            <input
             type="text"
             name="image"
             id="image"
             value={bike.image}
             onChange={handleInputChange}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          /> */}
+          />
           </div>
           <div className="mb-4 flex-1">
             <label
