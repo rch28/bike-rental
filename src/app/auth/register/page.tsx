ff093bda32b5e8d346bb5c93ba1e5b4ebc260e39
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-09 17:38:46 +0545

    update register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,6 @@
-import {  SignUpBg } from "@/assets";
-import Register from "@/components/Auth/Register";
+import { loginImage, SignUpBg } from "@/assets";
+import Register from "@/Auth/components/Register";
+import SignupProvider from "@/Auth/components/SignupProvider";
 import Layout from "@/components/global/Layout";
 import Image from "next/image";
 import React from "react";
@@ -8,22 +9,24 @@ const SignUpPage = () => {
   return (
     <div className="relative mb-16">
       <div className=" h-screen w-full overflow-hidden">
-        <Image
-          src={SignUpBg}
-          className="  w-full h-full object-cover"
-          alt=""
-        />
+        <Image src={SignUpBg} className="  w-full h-full object-cover" alt="" />
       </div>
       <div className=" w-full h-full absolute top-0">
         <Layout>
           {/* Register */}
-          <div className="max-w-lg mx-auto my-20">
-            <div className="bg-pink-100 p-6 rounded-xl">
-              <h1 className="text-4xl text-primary font-bold text-center tracking-widest">Sign Up</h1>
-              <p className="text-center text-gray-500">
+          <div className="flex gap-4 max-w-4xl  mx-auto   my-10 lg:my-12  bg-pink-200 p-6 rounded-xl">
+            <div className="flex-1 hidden md:flex justify-center items-center border-r-2 border-orange-500">
+              <Image
+                src={loginImage}
+                className="  w-full h-full object-cover mr-4"
+                alt=""
+              />
+            </div>
+            <div className="flex-1">
+              <p className=" text-center text-gray-800 text-xl font-semibold">
                 Create an account to get started
               </p>
-              <Register />
+              <SignupProvider />
             </div>
           </div>
         </Layout>
