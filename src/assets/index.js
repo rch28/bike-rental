8955e426ba28acc433b0c4677c7cb63127729d8a
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-09 10:34:00 +0545

    export login image

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,7 @@ import Money from "./logo/money.svg";
 import QualityService from "./logo/quality-service.svg";
 import Booking from "./logo/booking.svg";
 import Hobby11 from "./logo/bikes/hobby_11.jpg";
+import loginImage from "./logo/bikes/Login.png";
 
 import About from "./About.jpg";
 import LogoTransparent from "./logo/brslogot.png";
@@ -12,7 +13,7 @@ import LogoTransparent from "./logo/brslogot.png";
 import Contact from "./Contact.jpg";
 import GetInTouch from "./GetInTouch.jpg";
 
-import SignUpBg from "./SignUpBg.jpg"
+import SignUpBg from "./SignUpBg.jpg";
 
 export {
   WithMyBike,
@@ -21,9 +22,10 @@ export {
   QualityService,
   Booking,
   Hobby11,
+  loginImage,
   About,
   LogoTransparent,
   Contact,
   GetInTouch,
-  SignUpBg
+  SignUpBg,
 };
