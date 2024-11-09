32568dd6025442aa72f988d0a3f2e6223eda3822
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-09 09:56:02 +0545

    change text color border color

--- src/components/RHFComponents/RHFTextField.tsx
+++ src/components/RHFComponents/RHFTextField.tsx
@@ -1,8 +1,10 @@
 import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
 import { TextField, TextFieldProps } from "@mui/material";
+import { orange } from "@mui/material/colors";
+
 type Props<T extends FieldValues> = {
   name: Path<T>;
-} & Pick<TextFieldProps, "label">;
+} & Pick<TextFieldProps, "label" | "variant" | "type" | "placeholder">;
 
 const RHFTextField = <T extends FieldValues>({ name, ...props }: Props<T>) => {
   const { control } = useFormContext<T>();
@@ -17,6 +19,25 @@ const RHFTextField = <T extends FieldValues>({ name, ...props }: Props<T>) => {
           {...props}
           error={!!error}
           helperText={error?.message}
+          sx={{
+            "& label.Mui-focused": {
+              color: orange[700],
+            },
+            "& label": {
+              color: error && "red",
+            },
+            "& .MuiOutlinedInput-root": {
+              // "& fieldset": {
+              //   borderColor: error ? "red" : orange[500],
+              // },
+              "&:hover fieldset": {
+                borderColor: orange[700],
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: orange[700],
+              },
+            },
+          }}
         />
       )}
     />
