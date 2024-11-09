c1c23b52a32b10df9df08720374107194561b5c7
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-09 10:36:03 +0545

    feat : login schema with jod

--- src/Auth/types/LoginSchema.ts
+++ src/Auth/types/LoginSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email("Please enter a valid email address."),
-  password: z.string(),
+  password: z.string().min(1, "Password is required."),
 });
 
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
