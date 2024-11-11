d0f5d3172f52a01547cbcc306797d08cb26f0e76
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-11 21:15:34 +0545

    fix : mobile nav bar

--- src/components/global/MobileNavigation.tsx
+++ src/components/global/MobileNavigation.tsx
@@ -1,8 +1,9 @@
 import { Navlinks } from "@/lib/Navlink";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { FaX } from "react-icons/fa6";
+import { Button } from "../utils/Button";
 
 interface MobileNavigationProps {
   navbarWidth: string;
@@ -16,6 +17,17 @@ const MobileNavigation = ({
   navbarWidth,
 }: MobileNavigationProps) => {
   const pathname = usePathname();
+
+  useEffect(() => {
+    if (showNavBar) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => {
+      document.removeEventListener("mousedown", closeNavbar);
+    };
+  }, [showNavBar]);
   return (
     <div
       className={`bg-black/30 backdrop-blur-[2px] h-[100vh] w-[100vw] fixed top-0 left-0 z-[9999] flex justify-end overflow-hidden lg:hidden ${
@@ -33,17 +45,24 @@ const MobileNavigation = ({
 
         <ul className="flex flex-col gap-8 pt-20">
           {Navlinks.map((route, index) => (
-            <div key={index}>
+            <div key={index} className="px-6 ">
               <li
-                className={`font-medium font-lexend text-base text-center pb-7 ${
+                className={`font-medium font-lexend text-base text-center py-4 ${
                   pathname === route.url ? "text-red-600" : "text-black"
-                } hover:opacity-100 transition-opacity duration-300 ease-in-out cursor-pointer`}
+                } hover:opacity-100 transition-opacity duration-300 ease-in-out cursor-pointer hover:bg-primary hover:text-white  rounded-md`}
                 onClick={closeNavbar}
               >
                 <Link href={route.url}>{route.name}</Link>
               </li>
             </div>
           ))}
+          <div className="px-6">
+            <Button
+              title="Login"
+              path="/auth/login"
+              className="py-1.5  lg:py-[10px] md:hidden rounded-md lg:rounded-xl text-white "
+            />
+          </div>
         </ul>
       </div>
     </div>
