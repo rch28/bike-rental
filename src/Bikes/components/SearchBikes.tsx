1f00910a0225777af41d6de2d6c42c386ac94c55
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-22 20:05:12 +0545

    fix : search placeholder name

--- src/Bikes/components/SearchBikes.tsx
+++ src/Bikes/components/SearchBikes.tsx
@@ -14,7 +14,7 @@ const SearchBikes = () => {
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Search Yout Favourite Bikes"
+        placeholder="Search bike by name, model, brand"
         className="w-full py-2 px-6 border bg-white/50 border-gray-400 rounded-md outline-none focus-within:border-primary/75"
       />
       <LuSearch size={28} className="absolute right-4 top-2  text-gray-500 " />
