7066a4e0f2ffaef0eaadad023f1aa899aa46c596
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-23 21:35:48 +0545

    add fetch bike list

---
+++ src/Bikes/services/getBikeList.ts
@@ -0,0 +1,19 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import BikeServices from "./BikeServices";
+
+const getBikeList = (bikeId: string) => {
+  const { data, isFetching, refetch } = useQuery({
+    queryKey: ["bike", bikeId],
+    queryFn: async () => await BikeServices.getSingleBike(bikeId),
+  });
+  return {
+    data,
+    isFetching,
+    refetch,
+  };
+};
+
+export default getBikeList;
