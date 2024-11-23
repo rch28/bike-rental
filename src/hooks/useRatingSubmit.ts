9d7745a5b426b92306813226cb1f5af91411ff61
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-23 17:25:36 +0545

    feat : rating submit hook

---
+++ src/hooks/useRatingSubmit.ts
@@ -0,0 +1,63 @@
+import { successResponse } from "@/Auth/types/common";
+import BikeServices from "@/Bikes/services/BikeServices";
+import { RatingType } from "@/Bikes/types/RatingSchema";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useFormContext } from "react-hook-form";
+import toast from "react-hot-toast";
+
+const useRatingSubmit = (bikeId: string) => {
+  const router = useRouter();
+  const { handleSubmit, formState, reset } = useFormContext<RatingType>();
+  const me = localStorage.getItem("me");
+
+  const userId = me ? JSON.parse(me).id : null;
+  const onSubmit: SubmitHandler<RatingType> = async (data) => {
+    if (!me || !userId) {
+      toast.error("Please login to rate this bike");
+      router.push("/auth/login");
+      return;
+    }
+    const formData = {
+      ...data,
+      user: userId,
+      bike_id: bikeId,
+    };
+    const newPromise: Promise<successResponse> = new Promise(
+      async (resolve, reject) => {
+        try {
+          const response = await BikeServices.postRating(formData);
+          resolve(response);
+        } catch (error) {
+          if (error instanceof AxiosError && error.response?.data?.detail) {
+            const errMsg = error?.response?.data?.detail;
+            reject(errMsg);
+          } else if (error instanceof Error) {
+            reject(error.message);
+          } else {
+            reject("Network Error!!");
+          }
+        }
+      }
+    );
+
+    await toast.promise(newPromise, {
+      loading: "Submitting...",
+      success: (response) => {
+        if (response?.success) {
+          reset();
+        }
+        return response?.success || "Rating submitted successfully";
+      },
+      error: (err) => {
+        return err;
+      },
+    });
+  };
+  return {
+    handleSubmit: handleSubmit(onSubmit),
+    formState,
+  };
+};
+
+export default useRatingSubmit;
