e01d93517b76ec082a35139d7bc5118c2fc613d7
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-28 10:10:02 +0545

    add clear search

--- src/Bikes/components/SearchBikes.tsx
+++ src/Bikes/components/SearchBikes.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useBikeStore } from "@/store/bikeStore";
-import { LuSearch } from "react-icons/lu";
+import { LuSearch, LuX } from "react-icons/lu";
 
 const SearchBikes = () => {
   const { searchQuery, setSearchQuery } = useBikeStore((state) => ({
@@ -17,7 +17,18 @@ const SearchBikes = () => {
         placeholder="Search bike by name, model, brand"
         className="w-full py-2 px-6 border bg-white/50 border-gray-400 rounded-md outline-none focus-within:border-primary/75"
       />
-      <LuSearch size={28} className="absolute right-4 top-2  text-gray-500 " />
+      {searchQuery ? (
+        <LuX
+          size={24}
+          className="absolute right-4 top-2  text-gray-500 "
+          onClick={() => setSearchQuery("")}
+        />
+      ) : (
+        <LuSearch
+          size={28}
+          className="absolute right-4 top-2  text-gray-500 "
+        />
+      )}
     </div>
   );
 };
