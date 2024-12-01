bd8081d6370bf33dbc45b6a1749fe75cd759df75
Raju Chhetri <rajuchhetry11@gmail.com>
2024-12-01 22:39:37 +0545

    add service to rent bike

--- src/services/RentBikeServices.ts
+++ src/services/RentBikeServices.ts
@@ -1,11 +1,21 @@
 import { RentFormSearchSchemaType } from "@/BikeRent/types/RentFormSearchSchema";
 import configureAxios from "./axiosConfig";
+import { BikeRentFormSchemaType } from "@/BikeRent/types/BikeRentFormSchema";
+import { getCookies } from "./getCookies";
 
 const request = configureAxios();
 const RentBikeServices = {
   // Function to search  a bike on locations
   searchBikeOnLocation: async (data: RentFormSearchSchemaType) => {
     return request.post("rent/bike-search/", data);
+  },
+
+  // Function to rent a bike
+  rentBike: async (data: BikeRentFormSchemaType) => {
+    return request.post("rent/bike/", data, {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${await getCookies()}`,
+    });
   },
 };
 
