ea73873358a4bc51951f300f4f7e4102b86faca3
Raju Chhetri <rajuchhetry11@gmail.com>
2024-12-02 22:24:44 +0545

    navigate url on success

--- src/hooks/useRentBikeSubmit.ts
+++ src/hooks/useRentBikeSubmit.ts
@@ -3,7 +3,6 @@ import { AxiosError } from "axios";
 import { SubmitHandler, useFormContext } from "react-hook-form";
 import toast from "react-hot-toast";
 import { useNavigate } from "./navigate";
-import { useBikeStore } from "@/store/bikeStore";
 import {
   BikeRentFormSchemaType,
   RentalResponse,
@@ -11,17 +10,13 @@ import {
 
 const useRentBikeSubmit = () => {
   const { goTo } = useNavigate();
-  const { setBikes } = useBikeStore();
-
   const { handleSubmit, setValue, formState, watch } =
     useFormContext<BikeRentFormSchemaType>();
   const onSubmit: SubmitHandler<BikeRentFormSchemaType> = async (data) => {
-    console.log("data", data);
     const newPromise: Promise<RentalResponse> = new Promise(
       async (resolve, reject) => {
         try {
           const response = await RentBikeServices.rentBike(data);
-          console.log("response", response);
           resolve(response);
         } catch (error) {
           if (error instanceof AxiosError) {
@@ -40,7 +35,8 @@ const useRentBikeSubmit = () => {
     );
     await toast.promise(newPromise, {
       loading: "Renting bike...",
-      success: (data) => {
+      success: (d) => {
+        goTo(`/bike/rent/${data?.bike}/${d.id}`);
         return "Bike rented successfully";
       },
       error: (err) => err || "Failed to rent bike",
