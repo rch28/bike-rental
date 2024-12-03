80a90bddfc73bb84b78cdd6ad5c42969d0aa4bb0
Raju Chhetri <rajuchhetry11@gmail.com>
2024-12-03 19:38:03 +0545

    add loading icon

--- src/app/bike-on-rent/page.tsx
+++ src/app/bike-on-rent/page.tsx
@@ -1,6 +1,7 @@
 import BikeList from "@/Bikes/components/BikeList";
 import SearchBikes from "@/Bikes/components/SearchBikes";
 import Layout from "@/components/global/Layout";
+import Loading from "@/components/utils/Loading";
 import React, { Suspense } from "react";
 
 const BikeOnRentPage = () => {
@@ -12,7 +13,7 @@ const BikeOnRentPage = () => {
             <h1 className="text-primary">Bike On Rent</h1>
           </header>
           <SearchBikes />
-          <Suspense fallback={"loading.."}>
+          <Suspense fallback={<Loading />}>
             <BikeList />
           </Suspense>
         </div>
