0d7fbe393b1019fbd02663559b2b39f7ba603911
Raju Chhetri <rajuchhetry11@gmail.com>
2024-12-05 19:41:02 +0545

    store rental details

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,17 @@
+import { RentalResponse } from "@/BikeRent/types/BikeRentFormSchema";
 import { create } from "zustand";
 type State = {
   isLogged: boolean;
   translateSiderbar: string;
   toggleAddBikeDrawer: boolean;
+  rentalDetails: RentalResponse;
 };
 
 type Action = {
   setLogged: (value: boolean) => void;
   setTranslateSiderbar: (value: string) => void;
   setToggleAddBikeDrawer: (value: boolean) => void;
+  setRentDetails: (value: RentalResponse) => void;
 };
 export const useStore = create<State & Action>((set) => ({
   isLogged: false,
@@ -21,4 +24,8 @@ export const useStore = create<State & Action>((set) => ({
   // toggle AddBikeDrawer
   toggleAddBikeDrawer: false,
   setToggleAddBikeDrawer: (value) => set({ toggleAddBikeDrawer: value }),
+
+  // rent details
+  rentalDetails: {} as RentalResponse,
+  setRentDetails: (value) => set({ rentalDetails: value }),
 }));
